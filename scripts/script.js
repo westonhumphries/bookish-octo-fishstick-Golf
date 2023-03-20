36f8e764002472881cc8e434e88caf918cbef85b
kaibrysouthern <122912482+kaibrysouthern@users.noreply.github.com>
2023-03-20 10:52:01 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,6 @@
 $(function () {
   console.log("ready!");
 
-  // $.each(golfGames, (index) => {
-
-  //   // console.log("index:", index);
-  //   // console.log("value:", value);
-
-
-  //   $("#gameRow").append(`
-  //   <div class="col-sm-5 mx-auto mt-5">
-  //       <div class="card">
-  //         <h2 class="card-title"> ${golfGames[index].game} </h2>
-  //         <div class="card-body">${golfGames[index].author}</div>
-  //         <div class="card-footer">
-  //         <a target="_blank" href="${golfGames[index].Link}" class="btn btn-primary">Game 1</a>
-  //         <a target="_blank" href="${golfGames[index].Link2}" class="btn btn-primary">Southern Game</a>
-  //         <a target="_blank" href="${golfGames[index].Link3}" class="btn btn-primary">Stokes Game</a>
-  //         </div>
-  //       </div>
-  //     </div>
-  //     `);
-  // });
-
 
   $.each(golfGames2, (ind, val) => {
 
@@ -41,7 +20,7 @@ $(function () {
     <div class="col-sm-5 mx-auto mt-5">
         <div class="card">
           <h2 class="card-title"> ${val.game} </h2>
-          <div class="card-body"> "Game Authors:" ${val.authors[0]}", " ${val.authors[1]}", " ${val.authors[2]}</div>
+          <div class="card-body"> Game Author: ${val.authors[0]}, ${val.authors[1]}, ${val.authors[2]}</div>
           <div class="card-footer">${ link } 
           </div>
         </div>
